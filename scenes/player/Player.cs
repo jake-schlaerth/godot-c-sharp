39e887327172c6158fe26ff283d4630454e023d7
Jake Schlaerth <jakeschlaerth1999@gmail.com>
2026-09-21 17:34:36 -0400

    jump and tilemap

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -7,34 +7,35 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
-    public override void _Ready()
-    {
+	public override void _Ready()
+	{
 		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		_sprite.AnimationFinished += OnAnimationFinished;
 		_sprite.Play("idle");
-    }
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Read input once, then hand it to the helpers.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		bool jumpPressed = Input.IsActionJustPressed("ui_accept");
+		bool jumpHeld = Input.IsActionPressed("ui_accept");
+		bool jumpReleased = Input.IsActionJustReleased("ui_accept");
 
 		velocity = ApplyGravity(velocity, delta);
 
-		if (jumpPressed && IsOnFloor())
+		if (jumpReleased && IsOnFloor())
 		{
 			velocity = Jump(velocity);
 		}
 
 		velocity = Walk(direction.X, velocity);
-		UpdateAnimation(direction.X);
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		UpdateAnimation(direction.X, jumpHeld);
 	}
 
 	private Vector2 ApplyGravity(Vector2 velocity, double delta)
@@ -78,10 +79,10 @@ public partial class Player : CharacterBody2D
 			case "walk_end":
 				_sprite.Play("idle");
 				break;
-		}	
+		}
 	}
 
-	private void UpdateAnimation(float inputX)
+	private void UpdateAnimation(float inputX, bool jumpHeld)
 	{
 		bool isMoving = inputX != 0;
 
@@ -91,6 +92,34 @@ public partial class Player : CharacterBody2D
 		}
 
 		string currentAnimation = _sprite.Animation;
+		bool isInJumpAnimation = currentAnimation == "jump_start" || currentAnimation == "jump_loop";
+
+		if (!IsOnFloor())
+		{
+			if (currentAnimation != "jump_loop")
+			{
+				_sprite.Play("jump_loop");
+			}
+
+			return;
+		}
+
+		if (jumpHeld)
+		{
+			if (currentAnimation != "jump_start")
+			{
+				_sprite.Play("jump_start");
+			}
+
+			return;
+		}
+
+		if (isInJumpAnimation)
+		{
+			_sprite.Play(isMoving ? "walk_start" : "idle");
+			return;
+		}
+
 		bool isIdleOrEnding = currentAnimation == "idle" || currentAnimation == "walk_end";
 		bool isWalking = currentAnimation == "walk_start" || currentAnimation == "walk_loop";
 
